c9a96c0792013974291bff57bf3da01d278ae0f5
suvarna1228 <suvu1228@gmail.com>
2024-09-09 15:50:57 +0530

    program to check the number is palidrome or not

---
+++ Palindrome.py
@@ -0,0 +1,11 @@
+def palidrome(n):
+    revno=0
+    while (n>0):
+        last_digit=n%10
+        n=n//10
+        revno=revno*10 + last_digit
+    if (revno==n):
+       print("the number is palidrome")
+    else:
+        print("not a palidrome number") 
+palidrome(5467)  

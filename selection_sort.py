655245f24bea0aa77b869372ed124be7ed3b945b
suvarna1228 <suvu1228@gmail.com>
2024-09-13 13:05:59 +0530

    selection sort

--- selection_sort.py
+++ selection_sort.py
@@ -1,11 +1,12 @@
 def selectionsort(a):
-    n=len(a)
-    for i in range(n):
+   
+    for i in range(len(a)):
         min_index=i
-        for j in range(i+1,n):
+        for j in range(i+1,len(a)):
             if a[j]<a[min_index]:
               min_index=j
         a[i],a[min_index]=a[min_index],a[i]
+    return a
 a=[8,28,-2,4,-6]
 selectionsort(a)
 print("sorted arrey :",a)

91b98d0c01bb1516d887e903d44d49bde15c1d48
suvarna1228 <suvu1228@gmail.com>
2024-10-26 15:16:22 +0530

    matrix zeros

---
+++ set_matrix_zero.py
@@ -0,0 +1,34 @@
+def matrix_zero(a, n, m):
+    for i in range(n):
+        for j in range(m):
+            if a[i][j] == 0:
+                markRow(a, i, m)
+                markCol(a, j, n)
+    for i in range(n):
+        for j in range(m):
+            if a[i][j] == -1:
+                a[i][j] = 0
+
+def markRow(a, i, m):
+    for j in range(m):
+        if a[i][j] != 0:
+            a[i][j] = -1
+
+def markCol(a, j, n):
+    for i in range(n):
+        if a[i][j] != 0:
+            a[i][j] = -1
+
+matrix = [
+    [1, 2, 3],
+    [4, 0, 6],
+    [7, 8, 9]
+]
+
+n = len(matrix)
+m = len(matrix[0])
+
+matrix_zero(matrix, n, m)
+
+for row in matrix:
+    print(row)
